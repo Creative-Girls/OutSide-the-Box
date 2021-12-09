35aab58fe35e2930b38359f50bd06fb62cb58bde
Subin <80036437+hansb1023@users.noreply.github.com>
2021-12-09 15:55:05 +0900

    modify coinworld

---
+++ Main/CrossRiver/audio/CoinWorld/Player/Player.gd
@@ -0,0 +1,61 @@
+extends KinematicBody2D
+
+export var ACCELERATION = 500
+export var MAX_SPEED = 80
+export var FRICTION = 500
+
+enum {
+	MOVE
+}
+
+var state = MOVE
+var velocity = Vector2.ZERO
+
+var stats = PlayerStats
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+onready var hurtbox = $Hurtbox
+onready var blinkAnimationPlayer = $BlinkAnimationPlayer
+
+func _ready():
+	randomize()
+	stats.connect("no_health", self, "queue_free")
+	animationTree.active = true
+
+func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+	
+func move_state(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	move()
+
+func move():
+	velocity = move_and_slide(velocity)
+
+func _on_Hurtbox_area_entered(area):
+	stats.health -= area.damage
+	hurtbox.start_invincibility(0.6)
+	hurtbox.create_hit_effect()
+	
+func _on_Hurtbox_invincibility_started():
+	blinkAnimationPlayer.play("Start")
+
+func _on_Hurtbox_invincibility_ended():
+	blinkAnimationPlayer.play("Stop")
