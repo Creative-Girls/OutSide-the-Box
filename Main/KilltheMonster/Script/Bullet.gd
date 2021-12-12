395a9fc5efbc26175e4428a3641bf8e21b9f84b1
shiri24 <shiri24@ajou.ac.kr>
2021-12-13 05:04:28 +0900

    edit kill the monsteer
    edit kill the monsteer

---
+++ Main/KilltheMonster/Script/Bullet.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+const SPEED = 500
+var velocity = Vector2()
+var direction = 1
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+func set_bullet_direction(dir):
+	direction = dir
+	if dir == -1:
+		$Sprite.flip_h = true
+
+func _physics_process(delta):
+	velocity.x = SPEED * delta * direction
+	translate(velocity)
+	
+	if velocity.x > 0 || velocity.x < 0:
+		$Sprite.flip_h = velocity.x < 0
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+# 	Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_Bullet_body_entered(body):
+	if body.is_in_group("monster"):
+		body.hit()
+		queue_free()
+	#pass # Replace with function body.
--- Main/KilltheMonster/Script/Player.gd
+++ Main/KilltheMonster/Script/Player.gd
@@ -17,31 +17,39 @@ func _ready():
 
 func _physics_process(delta):
 	velocity.y += delta * GRAVITY
+	var bullet = BULLET.instance()
 	
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -WALK_SPEED
+		#bullet.velocity.x = -BULLET_SPEED*delta
+		if sign($Position2D.position.x) == 1:
+			$Position2D.position.x *= -1
+			
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x = WALK_SPEED
+		if sign($Position2D.position.x) == -1:
+			$Position2D.position.x *= -1
 	else:
 		$AnimatedSprite.animation = "idle"
 		# velocity.x = 0
 		# smoothen the stop
 		velocity.x = lerp(velocity.x, 0, 0.1)
+		#bullet.velocity.x = lerp(bullet.velocity.x, 0, 0.1)
 		
 	if Input.is_action_pressed("ui_up") and is_on_floor():
 		velocity.y = -JUMP_FORCE
-	else:
-		$AnimatedSprite.animation = "idle"
-		# velocity.x = 0
-		# smoothen the stop
-		velocity.x = lerp(velocity.x, 0, 0.1)
 	 
 	if Input.is_action_just_pressed("ui_focus_next"):
-		var bullet = BULLET.instance()
+		if sign($Position2D.position.x) == 1:
+			bullet.set_bullet_direction(1)
+		else:
+			bullet.set_bullet_direction(-1)
+		
 		get_parent().add_child(bullet)
-	
+		bullet.position = $Position2D.global_position
+		
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
+	#bullet.velocity = move_and_slide(velocity, Vector2.UP)	
 	
 	# prevent player going out of screen
 	position.x = clamp(position.x, 0, 3775)
@@ -56,9 +64,12 @@ func _physics_process(delta):
 		$AnimatedSprite.flip_v = false
 		# See the note below about boolean assignment
 		$AnimatedSprite.flip_h = velocity.x < 0
+
 	elif velocity.y > 0 || velocity.y < 0:
 		print("up in")
 		$AnimatedSprite.animation = "up"
 	elif velocity.x == 0 && velocity.y == 0:
 		$AnimatedSprite.animation = "idle"
 		#$AnimatedSprite.flip_v = velocity.y > 0
+
+	#pass # Replace with function body.
